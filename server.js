65621b3c7249536c6e12c0359f283ea4b985beca
Damir Vidaković <vida.damir@gmail.com>
2018-01-10 00:11:10 +0100

    making changes to app

--- server.js
+++ server.js
@@ -59,9 +59,16 @@ app.get('/', (req,res) => {
 //-GET REQUEST FOR CALC PAGE 
 app.get('/calc', (req,res) => {
   res.render('calc.hbs', {
-  pageTitle: 'HashPower Converter',
-  welcome: 'Here you can calculate hashpower', 
+  pageTitle: 'Sample calc',
+  welcome: 'Here you can calculate hashpower(limitation on H/s and Kh/s)', 
   });
+});
+
+app.get('/projects', (req,res) => {
+    res.render('projects.hbs', {
+        pageTitle:'Project Page',
+        welcome:'Some projects will be listed here'
+    });
 });
 
 
