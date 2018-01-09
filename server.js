e0a7d88c3f8209428208b15b57993c348c15a740
Damir Vidaković <vida.damir@gmail.com>
2018-01-09 14:33:54 +0100

    updating calc changes

--- server.js
+++ server.js
@@ -1,26 +1,63 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
-app.set('view engine', 'hbs')
-app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 
 
-
-app.get('/', (req,res) => {
-    res.render('home.hbs', {
-        pageTitle: 'Convert your HashPower',
-        welcome: 'Convert HashPower and calculate daily mined coins',
-        currentYear: new Date().getFullYear()
+//-express middleware
+app.use((req, res, next)=> {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(log);
+    //--write data to file
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log.');
+        }
     });
+    next();
 });
 
 
+//-GET REQUEST FOR MAINTENANCE PAGE
+/*app.use((req,res,next) => {
+    res.render('maintenance.hbs', {
+       pageTitle: 'Sorry server is down',
+       welcome: 'server down.. error 404'
+   })
+});*/
+ 
+//-ENTRY TO PUBLIC DIR
+app.use(express.static(__dirname + '/public'));
 
+//-HELPER FOR CUR YEAR
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+//-HELPER FOR UPPER CASE
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+//-GET REQUEST FOR HOME PAGE
+app.get('/', (req,res) => {
+    res.render('home.hbs', {
+        pageTitle: 'HashPower Coverter',
+        welcome: 'Convert HashPower and calculate daily mined coins',
+    });
+});
+
+//-GET REQUEST FOR CALC PAGE 
 app.get('/calc', (req,res) => {
   res.render('calc.hbs', {
   pageTitle: 'HashPower Coverter',
-  currentYear: new Date().getFullYear()  
+  welcome: 'Here you can calculate hashpower', 
   });
 });
 
